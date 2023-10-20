9c69382c3236dc1d46dc0726c865e2e4f408770a
RazikaBengana <razikabengana@gmail.com>
2023-10-20 14:48:33 +0200

    Update header file

--- main.h
+++ main.h
@@ -15,7 +15,10 @@ char *my_strcpy(char *dest, const char *src);
 char *my_strcat(char *dest, const char *src);
 size_t my_strlen(const char *str);
 char *my_strdup(const char *str);
+int my_strncmp(const char *s1, const char *s2, size_t n);
+int my_strcmp(const char *s1, const char *s2);
 char *my_getenv(const char *name);
+void exec_cd(char **argv);
 
 
 
