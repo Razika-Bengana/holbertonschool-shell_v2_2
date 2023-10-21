e5c99e86b99713517f93376e6b87ed047d18af04
RazikaBengana <razikabengana@gmail.com>
2023-10-21 15:50:51 +0200

    Fix cd command in exec_cd.c

--- exec_cd.c
+++ exec_cd.c
@@ -4,39 +4,49 @@ void exec_cd(char **argv)
 {
     char cwd[1024];
     char *oldpwd = my_getenv("OLDPWD");
-    char *pwd = my_getenv("PWD");
+    char *home = my_getenv("HOME");
 
     if (argv[1] == NULL)
     {
-        char *home = my_getenv("HOME");
         if (home != NULL)
         {
-            chdir(home);
+            if(chdir(home) != 0)
+            {
+                perror("cd");
+            }
         }
-        getcwd(cwd, sizeof(cwd));
-        my_setenv("PWD", cwd, 1);
-        my_setenv("OLDPWD", pwd, 1);
+        else
+        {
+            fprintf(stderr, "cd: HOME not set\n");
+        }
     }
     else if (my_strcmp(argv[1], "-") == 0)
     {
         if (oldpwd != NULL)
         {
-            chdir(oldpwd);
-            printf("%s\n", oldpwd);
+            if(chdir(oldpwd) != 0)
+            {
+                perror("cd");
+            }
         }
         else
         {
-            printf("%s\n", pwd);
+            fprintf(stderr, "cd: OLDPWD not set\n");
         }
-        getcwd(cwd, sizeof(cwd));
-        my_setenv("PWD", cwd, 1);
-        my_setenv("OLDPWD", pwd, 1);
     }
     else
     {
-        chdir(argv[1]);
-        getcwd(cwd, sizeof(cwd));
-        my_setenv("PWD", cwd, 1);
-        my_setenv("OLDPWD", pwd, 1);
+        if(chdir(argv[1]) != 0)
+        {
+            perror("cd");
+        }
+    }
+
+    getcwd(cwd, sizeof(cwd));
+    my_setenv("PWD", cwd, 1);
+
+    if (oldpwd != NULL)
+    {
+        my_setenv("OLDPWD", oldpwd, 1);
     }
 }
