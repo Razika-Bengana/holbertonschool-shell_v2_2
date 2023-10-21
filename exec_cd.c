ea8c1a4d025ca79fb462891fcf83a59c0a230188
RazikaBengana <razikabengana@gmail.com>
2023-10-21 15:36:38 +0200

    Fix issue with exec_cd handling of OLDPWD and "cd -"

--- exec_cd.c
+++ exec_cd.c
@@ -3,33 +3,40 @@
 void exec_cd(char **argv)
 {
     char cwd[1024];
-
-    char *oldpwd = my_getenv("PWD");
+    char *oldpwd = my_getenv("OLDPWD");
+    char *pwd = my_getenv("PWD");
 
     if (argv[1] == NULL)
     {
         char *home = my_getenv("HOME");
-
         if (home != NULL)
         {
             chdir(home);
         }
+        getcwd(cwd, sizeof(cwd));
+        my_setenv("PWD", cwd, 1);
+        my_setenv("OLDPWD", pwd, 1);
     }
     else if (my_strcmp(argv[1], "-") == 0)
     {
-        char *temp_oldpwd = my_getenv("OLDPWD");
-
-        if (temp_oldpwd != NULL)
+        if (oldpwd != NULL)
         {
-            chdir(temp_oldpwd);
+            chdir(oldpwd);
+            printf("%s\n", oldpwd);
         }
+        else
+        {
+            printf("%s\n", pwd);
+        }
+        getcwd(cwd, sizeof(cwd));
+        my_setenv("PWD", cwd, 1);
+        my_setenv("OLDPWD", pwd, 1);
     }
     else
     {
         chdir(argv[1]);
+        getcwd(cwd, sizeof(cwd));
+        my_setenv("PWD", cwd, 1);
+        my_setenv("OLDPWD", pwd, 1);
     }
-
-    getcwd(cwd, sizeof(cwd));
-    my_setenv("PWD", cwd, 1);
-    my_setenv("OLDPWD", oldpwd, 1);
 }
