718660d2d9c1fbb48629e16274386edb3a061af1
RazikaBengana <razikabengana@gmail.com>
2023-10-21 12:36:40 +0200

    Handle the cd - case

--- exec_cd.c
+++ exec_cd.c
@@ -5,12 +5,33 @@ void exec_cd(char **argv)
     if (argv[1] == NULL)
     {
         char *home = my_getenv("HOME");
+
         if (home != NULL)
         {
             if (chdir(home) != 0)
             {
                 perror("cd");
             }
+        }
+        else
+        {
+            fprintf(stderr, "cd: HOME environment variable not set.\n");
+        }
+    }
+    else if (my_strcmp(argv[1], "-") == 0)
+    {
+        char *oldpwd = my_getenv("OLDPWD");
+
+        if (oldpwd != NULL)
+        {
+            if (chdir(oldpwd) != 0)
+            {
+                perror("cd");
+            }
+        }
+        else
+        {
+            fprintf(stderr, "cd: OLDPWD environment variable not set.\n");
         }
     }
     else
